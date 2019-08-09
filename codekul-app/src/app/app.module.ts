8f3abb302762be7d458026ce8b1ffd587c0c3001
Dhekalepratibha <pratibhavinash98@gmail.com>
2019-08-08 23:40:09 -0700

    services : setup and registration

--- codekul-app/src/app/app.module.ts
+++ codekul-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServicesComponent } from './services/services.component';
+import { IotService } from './services/iot.service';
 
 @NgModule({
   declarations: [
